f59e5f65e79a65f4a8e8154ff4e5e767747df662
Alaminxab <alaminxab@gmail.com>
2022-09-17 00:45:30 +0100

    Deployment

--- components/Technologies.js
+++ components/Technologies.js
@@ -13,8 +13,8 @@ function Technologies() {
       <h1>IV. Interested Already?</h1>
       <p>I am currently looking for new Opportunities in order to grow and improve on my skills and tech stack with real world applications and Technologies</p>
       <p>Got a question? or Looking to reach out? Please don't hesitate </p>
-      <button><a href='mailto:alaminxab@gmail.com' target="_blank">SEND AN EMAIL</a></button>
-      <button><a href='https://drive.google.com/file/d/1pok_qZTxcZ49Myq2_ySHKlNSvbwViUxW/view?usp=drivesdk' target="_blank">VIEW MY RESUME</a></button>
+      <button><a href='mailto:alaminxab@gmail.com' target="_blank" rel="noreferrer">SEND AN EMAIL</a></button>
+      <button><a href='https://drive.google.com/file/d/1pok_qZTxcZ49Myq2_ySHKlNSvbwViUxW/view?usp=drivesdk' target="_blank" rel="noreferrer">VIEW MY RESUME</a></button>
     </div>
   )
 }
