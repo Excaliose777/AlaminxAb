dae6eb1a389750fd9266fe8a550aa05ed9fe7abf
Alaminxab <alaminxab@gmail.com>
2022-09-17 00:47:47 +0100

    Deployment

--- components/Technologies.js
+++ components/Technologies.js
@@ -5,14 +5,14 @@ function Technologies() {
   return (
     <div className={styles.main} id="tech">
       <h1>III. Technologies and Dev Stack</h1>
-      <p>I've worked with a number of Technologies and Tech Stack over time</p>
+      <p>I&apos;ve worked with a number of Technologies and Tech Stack over time</p>
       <p>My Tech stack include but not limited to...</p>
       <p className={styles.tech}>HTML/CSS/SASS,Tailwind, Javascript[ES6], ReactJs, NextJs, Solidity, Hardhat, EthersJs, OpenZeppelin, The Graph, RainbowKit, WAGMI, Chainlink, Ceramic...</p>
       <p>and some other Libraries and Programming Languages</p>
 
       <h1>IV. Interested Already?</h1>
       <p>I am currently looking for new Opportunities in order to grow and improve on my skills and tech stack with real world applications and Technologies</p>
-      <p>Got a question? or Looking to reach out? Please don't hesitate </p>
+      <p>Got a question? or Looking to reach out? Please don&apos;t hesitate </p>
       <button><a href='mailto:alaminxab@gmail.com' target="_blank" rel="noreferrer">SEND AN EMAIL</a></button>
       <button><a href='https://drive.google.com/file/d/1pok_qZTxcZ49Myq2_ySHKlNSvbwViUxW/view?usp=drivesdk' target="_blank" rel="noreferrer">VIEW MY RESUME</a></button>
     </div>
